7d32213427251bef265014ccffa083f01b0e7236
GUSTAVO PIETRO DE ASSIS SILVA <gustavo.silva2148@etec.sp.gov.br>
2025-08-08 14:54:55 -0300

    revisao completa

--- App.js
+++ App.js
@@ -1,30 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Dimensions } from 'react-native';
 import ImagemDog from './assets/casual_dog.png';
+import Google from './assets/Google.png';
+
+const { width } = Dimensions.get('window');
 
 export default function App() {
   return(
     <View style={styles.container}>
         <Image
          source={ImagemDog}
-         style= {{width:350 , height: 350}}
+         style= {styles.imagem}
+         resizeMode="contain"
          />
-      <Text>Ótimo dia!</Text>
-      <Text>Como deseja acessar?</Text>
+      <Text style={styles.textoInicial}>Ótimo dia!</Text>
+      <Text style={styles.textoInicial2}>Como deseja acessar?</Text>
       <StatusBar style="auto" />
 
       <TouchableOpacity
-                        style={{backgroundColor:'blue', padding: 20}}
-                        onPress={() => alert('Toque detectado')}
-      >  
-                          <Text>Como deseja acessar?</Text>                       
+                        style={styles.texto}
+                        onPress={() => alert('Google')} > 
+                        <Image source={Google} style= {styles.Google}></Image>
+                        <Text style={styles.textobotao}>Como deseja acessar?</Text>                       
       </TouchableOpacity>
 
       <TouchableOpacity
-                        style={{backgroundColor:'blue', padding: 20}}
-                        onPress={() => alert('Toque detectado')}
-      >  
-                          <Text>Outras opções</Text>                       
+                        style={styles.texto2}
+                        onPress={() => alert('Toque detectado')}  >  
+                        <Text style={styles.textobotao2} >Outras opções</Text>                       
       </TouchableOpacity>
 
     </View>
@@ -35,9 +38,74 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#F8F2C4',
+      backgroundColor: '#e9e9e9',
       alignItems: 'center',
       justifyContent: 'center',
      
     },
+    texto: {
+         backgroundColor:'#12b667',
+         fontFamily: "arial",
+         padding: 20,
+         marginTop: 10,
+         borderColor: 'black',
+         borderRadius: 10,
+         width: 330,
+         alignItems: 'center',
+         flexDirection: 'row',
+         justifyContent:'center',
+        
+    },
+    texto2: {
+      backgroundColor:'white',
+      padding: 20,
+      marginTop: 10,
+      borderColor: '#12b667',
+      borderRadius: 10,
+      width: 330,
+      borderWidth: 1,
+      alignItems: 'center',
+ },
+
+  textobotao:{
+       fontSize: 15,
+       color: 'white',
+       fontWeight: 'bold',
+        marginLeft: 5,
+       
+  },
+  textobotao2:{
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+  opacity: 0.5,
+},
+
+textoInicial:{
+  fontSize: 40,
+  marginBottom: 15,
+  marginTop: 15,
+  fontWeight:'bold',
+  opacity: 0.5, 
+},
+
+textoInicial2:{
+
+  marginBottom: 30,
+  fontSize: 16 ,
+  fontWeight: 'bold',
+  opacity: 0.5,
+  
+},
+
+imagem:{
+  width: width * 0.9,
+  height: width * 0.9,
+  marginBottom: 20,
+},
+
+Google:{
+  width: 30,
+  height: 30,
+},
   });
